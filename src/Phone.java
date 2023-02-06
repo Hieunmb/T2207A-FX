9d395b8a91e69e2aee761ea6542175783baf696a
Hieunmb <zhuongdae@gmail.com>
2023-02-06 16:47:20 +0700

    commit

---
+++ src/Phone.java
@@ -0,0 +1,28 @@
+public class Phone {
+    String name;
+    String telephone;
+
+    public Phone(String name, String telephone) {
+        this.name = name;
+        this.telephone = telephone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public String toString(){
+        return this.name+ "--"+this.telephone;
+    }
+}
--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,3 +1,68 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.util.Collections;
+import java.util.Comparator;
+
 public class PhoneNumber {
 
+    public TextField txtName;
+    public TextField txtTelephone;
+    public ListView<Phone> nb;
+    public ObservableList<Phone> listNumber = FXCollections.observableArrayList();
+    public Phone editNumber;
+    public boolean ascending=true;
+
+    public void submit(ActionEvent actionEvent) {
+        String n=txtName.getText();
+        String t=txtTelephone.getText();
+        if(editNumber==null){
+            Phone p= new Phone(n,t);
+            listNumber.add(p);
+        }else {
+            for(Phone p:listNumber){
+                if (p.getName().equals(editNumber.name) && p.telephone.equals(editNumber.telephone)) {
+                    p.setName(n);
+                    p.setTelephone(t);
+                }
+            }
+        }
+        nb.setItems(listNumber);
+        nb.refresh();
+        editNumber=null;
+        clearInput();
+    }
+    void clearInput(){
+        txtName.clear();
+        txtTelephone.clear();
+    }
+
+    public void editnb(MouseEvent mouseEvent) {
+        editNumber=nb.getSelectionModel().getSelectedItem();
+        txtName.setText(editNumber.getName());
+        txtTelephone.setText(editNumber.getTelephone());
+    }
+
+    public void sout(ActionEvent actionEvent) {
+        if(ascending){
+            Collections.sort(listNumber, new Comparator<Phone>() {
+                @Override
+                public int compare(Phone o1, Phone o2) {
+                    return o1.name.compareTo(o2.name);
+                }
+            });
+        }else {
+            Collections.sort(listNumber, new Comparator<Phone>() {
+                @Override
+                public int compare(Phone o1, Phone o2) {
+                    return o2.name.compareTo(o1.name);
+                }
+            });
+        }
+        ascending=!ascending;
+    }
 }
