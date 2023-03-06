ef2da1065b167cf2c9e33609d256d0aa9a27c8f2
Hieunmb <zhuongdae@gmail.com>
2023-03-06 15:21:57 +0700

    commit

--- src/exam/Main.java
+++ src/exam/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ListCustomer cus = new ListCustomer();
+        CustomerController cus = new CustomerController();
         Scanner sc = new Scanner(System.in);
         int select = 0;
         do {
